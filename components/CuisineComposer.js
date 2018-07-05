aedd2aef7963da225c2b4919bf915dd93068fed1
Ryan Hu <ryanhuuu@gmail.com>
2018-07-05 12:53:31 -0400

    finished stack nav for all tabs

--- components/CuisineComposer.js
+++ components/CuisineComposer.js
@@ -9,24 +9,24 @@ import Find from '../components/Find'
 import FindResults from '../components/FindResults'
 
 import Browse from '../components/Browse'
+import BrowseIngredients from '../components/BrowseIngredients'
+import BrowseRecipes from '../components/BrowseRecipes'
+
 import Cart from '../components/Cart'
+import CartActivate from '../components/CartActivate'
+import CartEdit from '../components/CartEdit'
+
 import Prep from '../components/Prep'
+import PrepFridge from '../components/PrepFridge'
+import PrepPlan from '../components/PrepPlan'
 
 import RecipeView from '../components/RecipeView'
+import IngredientView from '../components/IngredientView'
+
 
 import { createStackNavigator } from 'react-navigation';
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 
-
-
-const AddStack = createStackNavigator({
-  Add: Add,
-  AddIngredient: AddIngredient,
-  AddRecipe: AddRecipe,
-},
-{
-  initialRouteName: 'Add',
-});
 
 const FindStack = createStackNavigator({
   Find: Find,
@@ -37,7 +37,41 @@ const FindStack = createStackNavigator({
   initialRouteName: 'Find',
 });
 
+const AddStack = createStackNavigator({
+  Add: Add,
+  AddIngredient: AddIngredient,
+  AddRecipe: AddRecipe,
+},
+{
+  initialRouteName: 'Add',
+});
 
+const BrowseStack = createStackNavigator({
+  Browse: Browse,
+  BrowseIngredient: BrowseIngredients,
+  BrowseRecipe: BrowseRecipes,
+},
+{
+  initialRouteName: 'Browse',
+});
+
+const CartStack = createStackNavigator({
+  Cart: Cart,
+  CartActivate: CartActivate,
+  CartEdit: CartEdit,
+},
+{
+  initialRouteName: 'Cart',
+});
+
+const PrepStack = createStackNavigator({
+  Prep: Prep,
+  PrepFridge: PrepFridge,
+  PrepPlan: PrepPlan,
+},
+{
+  initialRouteName: 'Prep',
+});
 
 
 
