d104b688e31e442b217d3939c5cbf475c6dc9f1e
Ryan Hu <ryanhuuu@gmail.com>
2018-07-05 15:17:03 -0400

    added nav to cart

--- components/Cart.js
+++ components/Cart.js
@@ -1,59 +1,19 @@
 import React from 'react';
 import CartActivate from "./CartActivate"
 import CartEdit from "./CartEdit"
-import { Button, View, Text, Image } from "react-native";
+import { Button, View, Text } from "react-native";
 
 
 class Cart extends React.Component {
 
-  constructor(props){
-    super(props)
-
-    this.state = {
-      screen: ""
-    }
-
-  }
-
-
-  componentWillReceiveProps(){
-    this.setState({
-      screen: this.props.screen
-    })
-  }
-
-  display = () => {
-    switch(this.state.screen){
-      case "":
-        return this.showMainMenu()
-      case "edit":
-        return <CartEdit />
-      case "activate":
-        return <CartActivate />
-
-      default:
-        console.log("failed the switch")
-    }
-  }
-
-  setScreen = (word) => {
-    this.setState({
-      screen: word
-    })
-  }
-
-  showMainMenu(){
+  render() {
     return (
       <View>
         <Text>Cart</Text>
-        <Button className="addButtons" onPress={ () => this.setScreen('edit') } title="edit Cart"></Button>
-        <Button className="addButtons" onPress={ () => this.setScreen('activate') } title="Activate Cart"></Button>
+        <Button className="addButtons" onPress={ () => this.props.navigation.navigate('CartEdit') } title="edit Cart"></Button>
+        <Button className="addButtons" onPress={ () => this.props.navigation.navigate('CartActivate') } title="Activate Cart"></Button>
       </View>
     )
-  }
-
-  render() {
-    return  this.display()
   }
 }
 
